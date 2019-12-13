7291062d48bf5705cdfc0e735fed4311ab9cd3ff
Pravin Kumar <pravinkumar@classicinformatics.com>
2019-12-13 23:56:16 +0530

    update assignment module

--- packages/writer/src/containers/Assignments/Assignments.js
+++ packages/writer/src/containers/Assignments/Assignments.js
@@ -1,29 +1,29 @@
 import React from 'react';
 import { Tabs } from 'antd';
+import { useSelector, useDispatch } from 'react-redux';
 import Assignments from '@iso/containers/Assignments/Assignments';
 
-class ClientAssignments extends React.Component {
-  render() {
-    const { TabPane } = Tabs;
-    return (
-      <Tabs defaultActiveKey="1">
-        <TabPane tab="All" key="1">
-          <Assignments clearFilters={() => {}} clearAll={() => {}} />
-        </TabPane>
-        <TabPane tab="Ongoing Assignments" key="2">
-          <Assignments clearFilters={() => {}} clearAll={() => {}} />
-        </TabPane>
-        <TabPane tab="Submitted by Portal" key="3">
-          <Assignments clearFilters={() => {}} clearAll={() => {}} />
-        </TabPane>
-        <TabPane tab="Completed" key="4">
-          <Assignments clearFilters={() => {}} clearAll={() => {}} />
-        </TabPane>
-        <TabPane tab="Rejected" key="5">
-          <Assignments clearFilters={() => {}} clearAll={() => {}} />
-        </TabPane>
-      </Tabs>
-    );
-  }
+export default function Dashboard() {
+  const dispatch = useDispatch();
+  const appHeight = useSelector(state => state.App.height);
+  const { TabPane } = Tabs;
+  return (
+    <Tabs defaultActiveKey="1">
+      <TabPane tab="All" key="1">
+        <Assignments clearFilters={() => {}} clearAll={() => {}} />
+      </TabPane>
+      <TabPane tab="Ongoing Assignments" key="2">
+        <Assignments clearFilters={() => {}} clearAll={() => {}} />
+      </TabPane>
+      <TabPane tab="Submitted by Portal" key="3">
+        <Assignments clearFilters={() => {}} clearAll={() => {}} />
+      </TabPane>
+      <TabPane tab="Completed" key="4">
+        <Assignments clearFilters={() => {}} clearAll={() => {}} />
+      </TabPane>
+      <TabPane tab="Rejected" key="5">
+        <Assignments clearFilters={() => {}} clearAll={() => {}} />
+      </TabPane>
+    </Tabs>
+  );
 }
-export default ClientAssignments;
