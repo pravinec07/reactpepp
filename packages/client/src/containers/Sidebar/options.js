41b5b7996045a54ae464ec6649f215efafdc72a1
NehaTakhi <nehatakhi@gmail.com>
2019-12-24 14:42:40 +0530

    opyions

--- packages/client/src/containers/Sidebar/options.js
+++ packages/client/src/containers/Sidebar/options.js
@@ -4,5 +4,20 @@ const options = [
     label: 'sidebar.profile',
     leftIcon: 'ion-person',
   },
+  {
+    key: 'assignments',
+    label: 'Assignments',
+    leftIcon: 'ion-person',
+  },
+  {
+    key: 'project',
+    label: 'Project',
+    leftIcon: 'ion-android-checkbox-outline',
+  },
+  {
+    key: 'payment',
+    label: 'Payment',
+    leftIcon: 'ion-clipboard',
+  },
 ];
 export default options;
