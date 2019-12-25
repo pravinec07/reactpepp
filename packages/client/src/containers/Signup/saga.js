01a2f441556a55daf498cef1950088e7da177b09
NehaTakhi <nehatakhi@gmail.com>
2019-12-26 02:05:57 +0530

    redux

--- packages/client/src/containers/Signup/saga.js
+++ packages/client/src/containers/Signup/saga.js
@@ -6,13 +6,40 @@ import { signUp } from '../../services/usersApi';
 function* fetchSignupDataEffect() {
   try {
     yield takeEvery(actions.FETCH_START, function*(payload) {
-      let data = yield call(signUp, payload.payload);
-      yield put(actions.fetchSignUpSaveSuccess(data));
+      let response = yield call(signUp, payload.payload);
+      switch (response.status) {
+        case 200:
+          const status =
+            response.data.metadata && response.data.metadata.status
+              ? response.data.metadata.status
+              : 'SUCCESS';
+          console.log(status, '---->');
+          switch (status) {
+            case 'SUCCESS':
+              yield put({
+                type: actions.FETCH_SUCCESS,
+                token: response.data.accessToken,
+                profile: response.data,
+              });
+              break;
+            default:
+              yield put({
+                type: actions.FETCH_ERROR,
+                payload: response.data.errors[0].message.split('(')[0],
+              });
+              break;
+          }
+
+          break;
+        case 500:
+          yield put({ type: actions.FETCH_ERROR });
+          break;
+        default:
+          yield put({ type: actions.FETCH_ERROR });
+          break;
+      }
     });
-  } catch (error) {
-    console.log(error);
-    yield put(actions.getArticleError(error));
-  }
+  } catch (error) {}
 }
 export default function* rootSaga() {
   yield all([fork(fetchSignupDataEffect)]);
