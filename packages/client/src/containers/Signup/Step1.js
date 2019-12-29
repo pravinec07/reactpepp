dca02c1125eb5c63d1bb2646cb2efb403ce5f205
NehaTakhi <nehatakhi@gmail.com>
2019-12-30 01:14:33 +0530

    form

--- packages/client/src/containers/Signup/Step1.js
+++ packages/client/src/containers/Signup/Step1.js
@@ -1,10 +1,18 @@
 import React, { Fragment } from 'react';
-import { Form, Input, InputNumber, Row, Col, Button, Radio } from 'antd';
+import { Form, Input, Checkbox, Row, Col, Button, Radio } from 'antd';
 import Select, { SelectOption } from '@iso/components/uielements/select';
 
 export default function Step1(props) {
   const { getFieldDecorator } = props.data.form;
-  const { handleRadioChange, identityType, handleNextBackAction } = props;
+  const {
+    handleRadioChange,
+    identityType,
+    handleNextBackAction,
+    checkConfirm,
+    checkPassword,
+    handleConfirmBlur,
+    isAgreement,
+  } = props;
   const prefixSelector = getFieldDecorator('prefix', {
     initialValue: '+91',
   })(
@@ -52,6 +60,10 @@ export default function Step1(props) {
             {getFieldDecorator('email', {
               rules: [
                 {
+                  type: 'email',
+                  message: 'The input is not valid E-mail.',
+                },
+                {
                   required: true,
                   message: 'Please input your email',
                 },
@@ -72,6 +84,60 @@ export default function Step1(props) {
             })(<Input addonBefore={prefixSelector} />)}
           </Form.Item>
         </Col>
+        <Col xs={24}>
+          <Form.Item label="Password" hasFeedback>
+            {getFieldDecorator('password', {
+              rules: [
+                {
+                  required: true,
+                  message: 'Please input your password.',
+                },
+                {
+                  validator: checkConfirm,
+                },
+              ],
+            })(<Input />)}
+          </Form.Item>
+        </Col>
+        <Col xs={24}>
+          <Form.Item hasFeedback>
+            {getFieldDecorator('confirm', {
+              rules: [
+                {
+                  required: true,
+                  message: 'Please confirm your password.',
+                },
+                {
+                  validator: checkPassword,
+                },
+              ],
+            })(
+              <Input
+                type="password"
+                placeholder="Confirm Password"
+                onBlur={handleConfirmBlur}
+              />
+            )}
+          </Form.Item>
+        </Col>
+        <Col xs={24}>
+          <Form.Item>
+            {getFieldDecorator('agreement', {
+              rules: [
+                {
+                  required: true,
+                  message: 'Please select agreement',
+                },
+              ],
+            })(
+              <Checkbox name="agreement" onChange={handleRadioChange}>
+                {' '}
+                "By clicking Agree and Sign up, you agree to Pepper content's
+                Terms of use and Privacy Policy.
+              </Checkbox>
+            )}
+          </Form.Item>
+        </Col>
       </Row>
       <Row gutter={24}>
         <Col span={24} style={{ textAlign: 'right' }}>
@@ -79,6 +145,7 @@ export default function Step1(props) {
             type="primary"
             htmlType="button"
             onClick={handleNextBackAction}
+            disabled={!isAgreement}
           >
             Submit
           </Button>
