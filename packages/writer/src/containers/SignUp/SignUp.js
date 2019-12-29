4609556c367ede3416c42ccfbde8a36b5ba0fb48
Pravin Kumar <pravin.ec07@gmail.com>
2019-12-30 01:03:21 +0530

    stepsremoves

--- packages/writer/src/containers/SignUp/SignUp.js
+++ packages/writer/src/containers/SignUp/SignUp.js
@@ -5,23 +5,13 @@ import Button from '@iso/components/uielements/button';
 import Form from '@iso/components/uielements/form';
 import IntlMessages from '@iso/components/utility/intlMessages';
 import SignUpStyleWrapper from './SignUp.styles';
-import Steps from '@iso/components/uielements/steps';
-
 import Notification from '@iso/components/Notification';
 import userActions from '../../redux/user/actions';
-import Step1 from './step1';
-// import Step2 from "./step2";
-import Step3 from './step3';
-import Step4 from './step4';
-import Step5 from './step5';
-import Step6 from './step6';
-import Step7 from './step7';
+import SignUpForm from './step1';
 
 const { signUpRequest } = userActions;
 const FormItem = Form.Item;
-const { Step } = Steps;
 
-let formValues = {};
 function SignUp(props) {
   const dev = true;
   const [formStep, setFormStep] = React.useState(1);
@@ -31,12 +21,7 @@ function SignUp(props) {
     e.preventDefault();
     props.form.validateFieldsAndScroll((err, values) => {
       if (!err) {
-        formValues = { ...formValues, ...values };
-        if (formStep === 6) {
-          dispatch(signUpRequest(formValues));
-        } else {
-          setFormStep(formStep + 1);
-        }
+        dispatch(signUpRequest(values));
       }
     });
   };
@@ -129,34 +114,16 @@ function SignUp(props) {
               <IntlMessages id="page.signUpTitle" />
             </Link>
           </div>
-          <Steps
-            size="small"
-            current={formStep - 1}
-            style={{ marginBottom: 20 }}
-          >
-            <Step />
-            <Step />
-            <Step />
-            <Step />
-            <Step />
-            <Step />
-            <Step />
-          </Steps>
           <div className="isoSignUpForm">
             <Form layout="vertical" onSubmit={handleSubmit}>
-              {formStep === 1 && <Step1 form={props.form} dev={dev} />}
-              {formStep === 2 && <div>Otp Screen</div>}
-              {formStep === 3 && <Step3 form={props.form} dev={dev} />}
-              {formStep === 4 && <Step4 form={props.form} dev={dev} />}
-              {formStep === 5 && <Step5 form={props.form} dev={dev} />}
-              {formStep === 6 && <Step6 form={props.form} dev={dev} />}
-              {formStep === 7 ? (
-                <Step7 form={props.form} dev={dev} />
-              ) : (
-                <div className="isoInputWrapper isoLeftRightComponent">
-                  {actionBution(formStep)}
-                </div>
-              )}
+              <SignUpForm form={props.form} dev={dev} />
+              <>
+                <FormItem {...tailFormItemLayout}>
+                  <Button type="primary" htmlType="submit">
+                    <IntlMessages id="page.signUpButton" />
+                  </Button>
+                </FormItem>
+              </>
               <div className="isoInputWrapper isoCenterComponent isoHelperWrapper">
                 <Link to="/signin">
                   <IntlMessages id="page.signUpAlreadyAccount" />
