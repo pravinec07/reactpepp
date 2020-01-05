acc6967ecbc4a12cd75b0238f2b976f19b2bf8e6
Pravin Kumar <pravinkumar@classicinformatics.com>
2020-01-06 01:05:18 +0530

    dragger code break fixed

--- packages/writer/src/containers/Profile/CompleteProfile.js
+++ packages/writer/src/containers/Profile/CompleteProfile.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Icon, Row, Col, Card } from 'antd';
 import Upload from '@iso/components/uielements/upload';
 import Button from '@iso/components/uielements/button';
+import message from '@iso/components/uielements/message';
 import Checkbox from '@iso/components/uielements/checkbox';
 import Form from '@iso/components/uielements/form';
 import Select, { SelectOption } from '@iso/components/uielements/select';
@@ -20,7 +21,22 @@ import {
 const { Dragger } = Upload;
 const FormItem = Form.Item;
 const { updateProfileStart } = userActions;
-
+const UploadProps = {
+  name: 'file',
+  multiple: false,
+  action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+  onChange(info) {
+    const { status } = info.file;
+    if (status !== 'uploading') {
+      console.log(info.file, info.fileList);
+    }
+    if (status === 'done') {
+      message.success(`${info.file.name} file uploaded successfully.`);
+    } else if (status === 'error') {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+};
 function CompleteProfileDetails({ ...props }) {
   const { getFieldDecorator, getFieldsError } = props.form;
   const dispatch = useDispatch();
@@ -107,22 +123,17 @@ function CompleteProfileDetails({ ...props }) {
                     label=""
                     help="Uploading your CV/Resume adds a lot of credibility to your application and helps us evaluate better. Please upload only PDF files."
                   >
-                    {getFieldDecorator('dragger', {
-                      valuePropName: 'fileList',
-                      // getValueFromEvent: this.normFile,
-                    })(
-                      <Dragger name="files" action="/upload.do">
-                        <p className="ant-upload-drag-icon">
-                          <Icon type="inbox" />
-                        </p>
-                        <p className="ant-upload-text">
-                          Click or drag file to this area to upload
-                        </p>
-                        <p className="ant-upload-hint">
-                          Support for a single or bulk upload.
-                        </p>
-                      </Dragger>
-                    )}
+                    <Dragger {...UploadProps}>
+                      <p className="ant-upload-drag-icon">
+                        <Icon type="inbox" />
+                      </p>
+                      <p className="ant-upload-text">
+                        Click or drag file to this area to upload
+                      </p>
+                      <p className="ant-upload-hint">
+                        Support for a single or bulk upload.
+                      </p>
+                    </Dragger>
                   </Form.Item>
                 </Col>
               </Row>
@@ -297,22 +308,17 @@ function CompleteProfileDetails({ ...props }) {
                     label=""
                     help="The more, the merrier. We will only be able to assign you assignments in verticals and genre that you can prove you have previous experience in. And these samples help us pinpoint these verticals and categories!"
                   >
-                    {getFieldDecorator('dragger', {
-                      valuePropName: 'fileList',
-                      // getValueFromEvent: this.normFile,
-                    })(
-                      <Dragger name="files" action="/upload.do">
-                        <p className="ant-upload-drag-icon">
-                          <Icon type="inbox" />
-                        </p>
-                        <p className="ant-upload-text">
-                          Click or drag file to this area to upload
-                        </p>
-                        <p className="ant-upload-hint">
-                          Support for a single or bulk upload.
-                        </p>
-                      </Dragger>
-                    )}
+                    <Dragger {...UploadProps}>
+                      <p className="ant-upload-drag-icon">
+                        <Icon type="inbox" />
+                      </p>
+                      <p className="ant-upload-text">
+                        Click or drag file to this area to upload
+                      </p>
+                      <p className="ant-upload-hint">
+                        Support for a single or bulk upload.
+                      </p>
+                    </Dragger>
                   </Form.Item>
                 </Col>
               </Row>
